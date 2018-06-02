a1593547d399e51a1c6b7fc871eea8ca1c038f4f
barhoring <bar.apple@gmail.com>
2018-06-02 21:56:05 +0300

    displaying random cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,39 @@
 const express =require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data; //  data.cards
 
-router.get('/', function (req, res) {
+
+router.get('/:id', function (req, res) {
     //res.locals.prompt = "Who is burried in Grant's tomb?";
     // hint: "Think about who's tomb it is?"
-    res.render('card', {prompt: "Who is burried in Grant's tomb?"});
+    const { side } = req.query;
+    const { id } = req.params;
+    if(!Boolean(side)){ // !side 
+        res.redirect(`/cards/${id}?side=question`);
+    }
+
+    const name = req.cookies.username;  
+    const text = cards[id][side];
+    let templateData = { text, id, name};
+    templateData.sideToShow = 'question';  
+    if(side === "question"){
+        const { hint } = cards[id];
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+    }
+    res.render('card', templateData);
 });
+
+router.get('/', function(req, res){
+    const id = getRrandomId(cards.length - 1);
+    console.log(id);
+    res.redirect(`/cards/${ id }`);
+});
+
+function getRrandomId(max){
+    return Math.round(max * Math.random());
+}
 
 //module.exports = router;
 module.exports = router;
